2f25c80628daaf6178edbbd6d10a6c7dee016700
CataLozano <idkkhera@gmail.com>
2025-08-06 22:27:09 +0000

    Ejercicio 17 tiempo en recorrer distancia

---
+++ 16.py
@@ -0,0 +1,4 @@
+distancia= float(input("Ingrese la distancia en km: "))
+velocidad= float(input("Ingrese la velocidad en km/h: "))
+tiempo= distancia / velocidad
+print("El tiempo de viaje es: ", tiempo, "horas")
