4cfbbcc497c522f2bd4beda52ee951dac58e8d05
CataLozano <idkkhera@gmail.com>
2025-08-07 20:45:24 +0000

    ejercicio 19 elementos comunes y area circulo

--- 19.py
+++
@@ -1,3 +0,0 @@
-radio= int(input("Ingrese el radio del círculo: "))
-area= 3.1416 * radio * radio
-print(f"El área del círculo con radio {radio} es: {area:.2f}")
---
+++ 20.py
@@ -0,0 +1,3 @@
+radio= int(input("Ingrese el radio del círculo: "))
+area= 3.1416 * radio * radio
+print(f"El área del círculo con radio {radio} es: {area:.2f}")
